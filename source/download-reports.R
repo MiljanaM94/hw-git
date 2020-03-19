7c40bacdabc2a5cd45fa9e1149e307b538c3dd4e
ASUS <9miljana4@gmail.com>
2020-03-19 16:42:30 +0100

    Ucitaj fajl sa funkcijama i revidiraj kod

--- source/download-reports.R
+++ source/download-reports.R
@@ -1,13 +1,15 @@
 library(tidyverse)
+source("source/write-functions.R")
 
-year = c(2009:2020)
-quarter = c(1:4)
+year = 2009:2020
+quarter = 1:4
 
 df <- crossing(year, quarter) %>% 
   filter(
-    (year != 2009 | quarter != c(1,2,3)) &
-      (year!= 2020 | quarter != c(2,3,4))
+    (year != 2009 | quarter != 1:3) &
+      (year!= 2020 | quarter != 2:4)
   )
+
 for(i in seq_len(nrow(df))) {
   download_report(df$year[i], df$quarter[i])
 }
