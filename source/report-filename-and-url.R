915686e581111e3fc65a36c367828c327cdebe8f
ASUS <9miljana4@gmail.com>
2020-03-19 12:58:50 +0100

    Fix #1 Odredi naziv Excel fajla i njegovog URL-a

---
+++ source/report-filename-and-url.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+
+report_filename <- function(year, quarter) {
+  quarter <- case_when(
+    quarter == 1 ~ "02",
+    quarter == 2 ~ "05",
+    quarter == 3 ~ "08",
+    quarter == 4 ~ "11"
+  )
+  extension <- if_else(year <= 2018, ".xls", ".xlsx")
+  result <- if_else(
+    year == 2009,
+    "2009_11_4_projekcija_inflacije.xls",
+    paste0(year, "_", quarter, "_5_projekcija_inflacije", extension)
+  )
+  result
+}
+
+report_url <- function(year, quarter) {
+  result <- paste0(
+    "https://www.nbs.rs/static/nbs_site/gen/latinica/90/ioi/ioi_", year, "/",
+    report_filename(year, quarter)
+  )
+  result
+}
